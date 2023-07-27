9504e642ef8cf8584377ce388b1cfbbcd46fd9fc
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-07-28 02:36:08 +0530

    added Darkandlight Mode

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,18 +1,75 @@
+"use client"
+
+import { useEffect } from "react";
 
 
 export default function Header() {
+
+    // Header Sticky Animation //
+    useEffect(() =>{
+        window.addEventListener('scroll', isSticky);
+    return () =>{
+        window.removeEventListener('scroll', isSticky);
+    }
+    });
+    const isSticky = () =>{
+        const header = document.querySelector(".Header-section");
+        const scrollTop = window.scrollY;
+        scrollTop >= 200 ? header.classList.add('isSticky'):header.classList.remove('isSticky');
+    }
+    // $$
+
+    // local storage for saving the visits
+    // click for saved 'darkMode' in localStorage
+    let darkMode = localStorage.getItem('darkMode');
+
+    //To enabled DarkTheme function
+    const enableDarkTheme = () => {
+        // 1 add the class to the body
+        document.body.classList.add('darkmode');
+
+        // 2 Update darkMode in the localStorage
+        localStorage.setItem('darkMode','enabled');
+    }
+
+    const disableDarkTheme = () => {
+        // 1 remove the class to body
+        document.body.classList.remove('darkmode');
+
+        // 2 update in localstorage
+        localStorage.setItem('darkMode', null);
+    }
+
+    // If the user already visited and enabled darkmode
+    // start things off with it on
+    if(darkMode === "enabled"){
+        enableDarkTheme();    
+    }
+
+    //when user click the button
+    // Theme Toggle
+    const DarkTheme = () => {
+        darkMode = localStorage.getItem('darkMode');
+
+        if(darkMode !== "enabled"){
+            enableDarkTheme();
+        }
+        else{
+            disableDarkTheme();
+        }
+    
+    }
+    // $$
     return(
-        <div className='Header'>
+        <div className='Header Header-section'>
             <div className="icon"><h4>icon</h4></div>
             <div className="SideNAV">
                     <li><h4>Home</h4></li>
                     <li><h4>About</h4></li>
                     <li><h4>Portfolio</h4></li>
                     <li><h4>Contact</h4></li>
-                    <li><h4>Contact</h4></li>
-                    <li><h4>Contact</h4></li>
                     <div>
-                        <h4>Toggle</h4>
+                        <h4 onClick={DarkTheme}>Toggle</h4>
                     </div>
             </div>
                 
