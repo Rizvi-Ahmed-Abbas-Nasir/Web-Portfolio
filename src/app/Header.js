a3d7f0206457e2658bded23fc8e92ce4bc3bb236
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-07-27 14:36:20 +0530

    changs to header

--- src/app/Header.js
+++ src/app/Header.js
@@ -3,13 +3,20 @@
 export default function Header() {
     return(
         <div className='Header'>
-            <div>icon</div>
-            <div className="List">
-                <li>Exprence</li>
-                <li>Exprence</li>
-                <li>Exprence</li>
-                <li>Exprence</li> 
-            </div>            
+            <div className="icon"><h4>icon</h4></div>
+            <div className="SideNAV">
+                    <li><h4>Home</h4></li>
+                    <li><h4>About</h4></li>
+                    <li><h4>Portfolio</h4></li>
+                    <li><h4>Contact</h4></li>
+                    <li><h4>Contact</h4></li>
+                    <li><h4>Contact</h4></li>
+                    <div>
+                        <h4>Toggle</h4>
+                    </div>
+            </div>
+                
+           
         </div>
     );
 }   
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 export default function Home(){
   return(
   <>
-    <h1>Abbas</h1>
+    <div className="homePage">
+    
+    </div>
   </>
 
 
