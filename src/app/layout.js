3ae4f94ecbf8288912da00e322df4735858684f7
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-08-17 15:12:07 +0530

    add backgorund aniamtion

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      </head>
       <body className={roboto.className}>
         <Header />
         <div>{children}</div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,94 @@
-export default function Home(){
-  return(
-  <>
-    <div className="homePage">
-    
-    </div>
-  </>
+"use client"
 
+import { useCallback } from "react";
+import Particles from "react-particles";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
+export default function Home() {
 
-  );
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        await loadSlim(engine);
+    }, []);
 
-}
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
+    return (
+      <div className="homePage">
+        <div className="mainPage">
+        {<Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "0",
+                    },
+                },
+                fpsLimit: 90,
+                interactivity: {
+                    events: {
+                        // onClick: {
+                        //     enable: true,
+                        //     mode: "push",
+                        // },
+                        onhover: {
+                          enable: true,
+                          mode: "attract",
+                          parallax: { enable: true, force: 60, smooth: 10 }
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                     
+                      // attract: { distance: 20, duration: 4, factor: 5 }
+                    }
+                },
+                particles: {
+                    color: {value:["#ffffff"]
+                  },
+                    links: {
+                       color: {value:["#181c28"]},
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 1000,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 1,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 3 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        /> }
+        </div>
+      </div>
+    );
+};
