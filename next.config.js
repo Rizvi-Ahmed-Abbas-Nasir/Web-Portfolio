6c8b3a16e7b42e62593c64e18e52cd63a97f0879
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-08-22 04:48:54 +0530

    completed the front page

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const path = require('path')
-
-const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-}
+const nextConfig = {  output: 'export', }
 
 module.exports = nextConfig
