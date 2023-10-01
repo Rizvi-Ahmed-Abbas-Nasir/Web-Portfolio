5f245feb5451929ef90d429c2b542c2052aabac5
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-10-01 16:55:44 +0530

    added about page section

--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,8 @@ import insta from './Images/insta.png';
 import linkedin from "./Images/linkedin.png"
 import github from "./Images/github.png";
 import Typewriter from 'typewriter-effect';
+import FrontPage from './pages/FrontPage';
+import AboutPageSection from './pages/AboutPageSection';
 
 
 // Fonts Families
@@ -42,122 +44,15 @@ const courgette = Courgette({
 
 export default function Home() {
 
-    const particlesInit = useCallback(async engine => {
-        console.log(engine);
-        await loadSlim(engine);
-    }, []);
-
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-    }, []);
-
     return (
       <div className="homePage">
-        <div className="mainPage">
-        {<Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "0",
-                    },
-                },
-                fpsLimit: 90,
-                interactivity: {
-                    events: {
-                        // onClick: {
-                        //     enable: true,
-                        //     mode: "push",
-                        // },
-                        onhover: {
-                          enable: true,
-                          mode: "attract",
-                          parallax: { enable: true, force: 60, smooth: 10 }
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                     
-                      // attract: { distance: 20, duration: 4, factor: 5 }
-                    }
-                },
-                particles: {
-                    color: {value:["#ffffff"]
-                  },
-                    links: {
-                       color: {value:["#181c28"]},
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 0.4,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 1000,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 1,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 2 },
-                    },
-                },
-                detectRetina: true,
-            }}
-        /> }
-
-        <div className="frontPage">
-            <div className="myPhoto">
-                <Image className="rizvi" 
-                    src={abbas}
-                />
-
-            </div>
-
-            <div className="Descriptions">
-                <div className={courgette.className} id='Introduction'>
-                <p>hello there,</p><p> My Name is </p>
-                </div>
-                <div id="MyName" className={oswald.className}> 
-               <h2>Rizvi Ahmed Abbas</h2>
-                </div>
-                <div id='Multiples-skills-text' className={rokkitt.className} > 
-                    <h4>I'm a Professional <div className='MultiText'> <Typewriter 
-                                                options={{
-                                                        strings: [
-                                                        'MERN Stack Developer', 
-                                                        'Python Developer',
-                                                        'Cloud Engineer'
-                                                        ],
-                                                        autoStart: true,
-                                                        loop: true,
-                                                    }}/></div></h4>
-                </div>
-                <div id='downloadAndContactBTN'>
-                    <button>Downlaod CV</button>
-                    <button>About Me</button>
-                </div>
-            </div>
+       <FrontPage />
+       <div className='content'>
+        <div className='AboutPageSection'>
+            <AboutPageSection />
         </div>
-        </div>
+       </div>
+       
       </div>
     );
 };
---
+++ src/app/pages/AboutPageSection.js
@@ -0,0 +1,58 @@
+
+"use client"
+import { Raleway } from 'next/font/google';
+
+
+
+
+// Fonts
+const raleway = Raleway({ 
+    subsets: ['latin'],
+    weight: ['400'],
+    //style: ['italic'] 
+})
+
+const raleway2 = Raleway({ 
+    subsets: ['latin'],
+    weight: ['400'],
+    //style: ['italic'] 
+})
+// ##
+
+export default function AboutPageSection() {
+
+    const observer = new IntersectionObserver((entries) =>{
+        entries.forEach((entry) =>{
+          console.log(entry);
+          if(entry.isIntersecting){
+            entry.target.classList.add('show');
+          }
+          else{
+            entry.target.classList.remove("show")
+          }
+        })
+      } )
+    
+      const hiddenElements = document.querySelectorAll(".hidden");
+     
+      hiddenElements.forEach((el) => observer.observe(el));
+    
+
+    return(
+        <div className="AboutME">
+            <div className="AboutME_Contents">
+            <div className={raleway.className} id='Greeting'>
+            <h1 className='hidden' >Hii there, Nice to meet you!!</h1> 
+            <h2 className='hidden'>Thank Your For Visiting My Web Portfolio</h2>
+            <p className='hidden'> Welcome!! my fellow devs, it's an honour for me, a Professional dev like yourself is reviewing my
+            porfolio if you came here i am assuming you wanted to see my skills, projects and my Professional Works, Hii My Name is Rizvi Ahmed Abbas i am Computer Engineer Student Currently Pursuring Bacholar In Computer (BE)
+            and a MERN Stack, Python Developer i had work as an intern in ElectricBikeWale as an Full-stack developer and Hasbasoft as an MERN Stack Developer, also i had state level winner in my college for my project AI Machine 
+            Vision Desktop Controller for more detial please review my full porfolio
+            </p>
+            </div>
+            </div>
+        </div>
+    );
+
+
+}
